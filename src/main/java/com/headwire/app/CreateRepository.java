9fbef8540b7904c7eb25b9f90835083c481208fe
Li Qin <lq@headwire.com>
2018-10-10 11:01:32 -0700

    constructor: initial check in

--- src/main/java/com/headwire/app/CreateRepository.java
+++ src/main/java/com/headwire/app/CreateRepository.java
@@ -1,5 +1,13 @@
 package com.headwire.app;
 
+import javax.jcr.Node;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+
+import org.apache.jackrabbit.oak.Oak;
+import org.apache.jackrabbit.oak.jcr.Jcr;
 
 
 /**
@@ -7,10 +15,39 @@ package com.headwire.app;
  */
 public class CreateRepository {
 
-    
-
     public CreateRepository() {
         
+    	// Create repository
+    	Repository repo = new Jcr(new Oak()).createRepository();
+    	
+    	// Login to repository
+    	Session session = null;
+    	
+    	try {
+			session = repo.login(
+			        new SimpleCredentials("admin", "admin".toCharArray()));
+			Node root = session.getRootNode();
+	        if (root.hasNode("hello")) {
+	            Node hello = root.getNode("hello");
+	            long count = hello.getProperty("count").getLong();
+	            hello.setProperty("count", count + 1);
+	            System.out.println("found the hello node, count = " + count);
+	        } else {
+	            System.out.println("creating the hello node");
+	            root.addNode("hello").setProperty("count", 1);
+	        }
+	        session.save();
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+		}
+        
+    	// Todo: add nodes
+    	
+    	// Logout from repository
+    	session.logout();
+
+    	
+    	
     }
 
     
